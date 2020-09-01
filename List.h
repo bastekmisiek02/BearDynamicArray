b08a5d6c3238516a911a0c9e522fcb3e80bc28bd
bastekmisiek02 <bastek.misiek2002@gmail.com>
2020-09-01 14:34:47 +0200

    Rework

--- List.h
+++ List.h
@@ -11,88 +11,59 @@ namespace Bear
 	{
 		typedef unsigned long long int BearListLongInt;
 
-		struct ListElements
-		{
-			ListElements()
-				:nextElement(nullptr), previewElement(nullptr)
-			{
-			}
-
-			ListElements(ListElements* NextElement, ListElements* PreviewElement, const T* Element)
-				: nextElement(NextElement), previewElement(PreviewElement), element((T*)Element)
-			{
-			}
-
-			~ListElements()
-			{
-				nextElement = nullptr;
-				previewElement = nullptr;
-
-				element = nullptr;
-			}
-
-			T* element;
-
-			ListElements* nextElement;
-			ListElements* previewElement;
-		};
-
 	private:
 		BearListLongInt count;
 	private:
-		ListElements* firstElement;
-		ListElements* lastElement;
+		T* items;
 	private:
-		const T* GetFromIndex(const BearListLongInt& index) const
+		const T& GetFromIndex(const BearListLongInt& index) const
 		{
-			#ifndef BEAR_LIST_NOT_EXCEPTIONS
-				if (index > count - 1)
-					throw std::exception("Index is greater than count");
-				
-				if (index < 0)
-					throw std::exception("Index must be greater than 0");
-				
-				if (!count)
-					throw std::exception("List is clear");
-			#endif
+			if (index > count - 1)
+				throw std::exception("Index is greater than count");
 
-			BearListLongInt i = 0;
+			if (index < 0)
+				throw std::exception("Index must be greater than 0");
 
-			ListElements* le = firstElement;
+			if (!count)
+				throw std::exception("List is clear");
 
-			while (le != nullptr)
-			{
-				if (i == index)
-					return le->element;
-
-				i++;
-				le = le->nextElement;
-			}
-
-			return nullptr;
+			return items[index];
 		}
 	public:
 		#if __has_include(<vector>)
-		List(const std::vector<T>& elements)
-		{
-			for (BearListLongInt i = 0; i < elements.size(); i++)
+			List(const std::vector<T>& Elements)
+				: count(Elements.size())
 			{
-				Add(elements[i]);
+				items = new T[Elements.size()];
+
+				for (BearListLongInt i = 0; i < Elements.size(); i++)
+				{
+					items[i] = Elements[i];
+				}
 			}
-		}
 		#endif
 
-		List(const List<T>& elements)
+		List(const List<T>& Elements)
+			: count(Elements.count)
 		{
-			for (BearListLongInt i = 0; i < elements.count; i++)
+			items = new T[Elements.count];
+
+			for (BearListLongInt i = 0; i < Elements.count; i++)
 			{
-				Add(*elements.GetFromIndex(i));
+				items[i] = Elements[i];
 			}
+		}
+
+		List(const BearListLongInt& Count)
+			: count(Count)
+		{
+			items = new T[Count];
 		}
 
 		List()
-			: count(0), firstElement(nullptr), lastElement(nullptr)
+			: count(0)
 		{
+			items = new T[0];
 		}
 
 		~List()
@@ -100,149 +71,90 @@ namespace Bear
 			Clear();
 		}
 	public:
+		const bool& Exist(const T& element)
+		{
+			for (BearListLongInt i = 0; i < count; i++)
+			{
+				if (items[i] == element)
+					return true;
+			}
+
+			return false;
+		}
+
 		void Add(const T& Element)
 		{
-			switch (count)
-			{
-				case 0:
-				{
-					firstElement = new ListElements(nullptr, nullptr, &Element);
-					lastElement = firstElement;
-					count++;
-					break;
-				}
-				case 1:
-				{
-					firstElement->nextElement = new ListElements(nullptr, firstElement, &Element);
-					lastElement = firstElement->nextElement;
-					count++;
-					break;
-				}
-				default:
-				{
-					lastElement->nextElement = new ListElements(nullptr, lastElement, &Element);
-					lastElement = lastElement->nextElement;
-					count++;
-					break;
-				}
-			}
+			T* array = new T[count];
+
+			for (BearListLongInt i = 0; i < count; i++)
+				array[i] = this->items[i];
+
+			delete[] this->items;
+
+			this->items = new T[count + 1];
+
+			for (BearListLongInt i = 0; i < count; i++)
+				this->items[i] = array[i];
+
+			this->items[count] = Element;
+
+			delete[] array;
+
+			count++;
 		}
 
-		const bool& Delete(const T& Element)
+		void Delete(const T& Element, const bool& checkToExist = true)
 		{
-			switch (count)
+			if (!items || !count)
+				throw std::exception("List is clear");
+
+			if (checkToExist)
 			{
-				case 0:
-					throw std::exception("List is clear");
-				case 1:
-				{
-					if (*firstElement->element == Element)
-					{
-						delete firstElement;
-						firstElement = nullptr;
-						count--;
-					}
-					return true;
-				}
-				case 2:
-				{
-					if (*firstElement->element == Element)
-					{
-						delete firstElement;
-						firstElement = lastElement;
-
-						lastElement = nullptr;
-					}
-					else
-					{
-						delete lastElement;
-						lastElement = nullptr;
-
-						firstElement->nextElement = nullptr;
-					}
-
-					count--;
-
-					return true;
-				}
-				default:
-				{
-					if (*firstElement->element == Element)
-					{
-						firstElement->element = nullptr;
-						firstElement->nextElement->previewElement = nullptr;
-
-						ListElements* element = firstElement->nextElement;
-						delete firstElement;
-
-						firstElement = element;
-					}
-					else if (*lastElement->element == Element)
-					{
-						lastElement->element = nullptr;
-						lastElement->previewElement->nextElement = nullptr;
-
-						ListElements* element = lastElement->previewElement;
-						delete lastElement;
-
-						lastElement = element;
-					}
-					else
-					{
-						ListElements* element = firstElement;
-
-						while (element->nextElement != nullptr)
-						{
-							if (*element->element == Element)
-							{
-								ListElements* previewElement = element->previewElement;
-								ListElements* nextElement = element->nextElement;
-
-								previewElement->nextElement = nextElement;
-								nextElement->previewElement = previewElement;
-
-								delete element;
-
-								break;
-							}
-
-							element = element->nextElement;
-						}
-
-						if (element == lastElement)
-							return false;
-					}
-
-					count--;
-
-					return true;
-				}
+				if (!Exist(Element))
+					throw std::exception("Can't find this element");
 			}
-		}
 
-		void DeleteFromIndex(const int& index)
-		{
-			Delete(*(GetFromIndex(index)));
+			T* array = new T[count - 1];
+
+			int j = 0;
+			int removeItems = 0;
+			for (BearListLongInt i = 0; i < count; i++)
+			{
+				if (this->items[i] != Element)
+				{
+					array[j] = this->items[i];
+					j++;
+				}
+				else
+					removeItems++;
+			}
+
+			delete[] this->items;
+
+			count -= removeItems;
+
+			this->items = new T[count];
+
+			for (BearListLongInt i = 0; i < count; i++)
+				this->items[i] = array[i];
+
+			delete[] array;
 		}
 
 		T& GetFirst() const
 		{
-			#ifndef BEAR_LIST_NOT_EXCEPTIONS
-				if (!count)
-					throw std::exception("First Element is null");
-			#endif
+			if (!count)
+				throw std::exception("First Element is null");
 
-			return *firstElement->element;
+			return items[0];
 		}
 
 		T& GetLast() const
 		{
-#			ifndef BEAR_LIST_NOT_EXCEPTIONS
-				if (!count)
-					throw std::exception("Last Element is null");
-			#endif
+			if (!count)
+				throw std::exception("Last Element is null");
 
-			return *lastElement->element;
+			return items[count - 1];
 		}
 
 		const BearListLongInt& GetMaxCount() const
@@ -257,61 +169,44 @@ namespace Bear
 
 		void Clear()
 		{
-			while (count)
-			{
-				ListElements* element = lastElement->previewElement;
-				delete lastElement;
-
-				lastElement = element;
-				count--;
-			}
-
-			firstElement = nullptr;
+			delete[] items;
+			items = nullptr;
+			count = 0;
 		}
 
-		void Sort(const bool& (*SortFunc)(const T& firstElement, const T& secondElement)) const
+		T* Data() const
 		{
-			#ifndef BEAR_LIST_NOT_EXCEPTIONS
-			if (!count)
-				throw std::exception("List is clear");
-			#endif
-
-			if (count == 1)
-				return;
-
-			ListElements* firstElement = this->firstElement;
-
-			List<T> list;
-			list.Add(*firstElement->element);
-
-			for (BearListLongInt i = 1; i < count; i++)
-			{
-				firstElement = firstElement->nextElement;
-				ListElements* secondElement = firstElement;
-
-				list.Add(*secondElement->element);
-
-				ListElements* lePtr = firstElement;
-
-				for (BearListLongInt j = 0; j < list.count - 1; j++)
-				{
-					if (!(SortFunc(*lePtr->previewElement->element, *secondElement->element)))
-					{
-						const T helpItem = *lePtr->previewElement->element;
-						*lePtr->previewElement->element = *secondElement->element;
-						*secondElement->element = helpItem;
-
-						lePtr = lePtr->previewElement;
-						secondElement = lePtr;
-					}
-				}
-			}
+			return items;
 		}
+
+		void Resize(const BearListLongInt& Count)
+		{
+			if(items)
+				delete[] items;
+
+			items = new T[Count];
+			
+			count = Count;
+		}
+
+		#if __has_include(<vector>)
+		std::vector<T> ToVector() const
+		{
+			std::vector<T> vector(count);
+
+			for (BearListLongInt i = 0; i < count; i++)
+			{
+				vector[i] = items[i];
+			}
+
+			return vector;
+		}
+		#endif
 
 	public:
-		T& operator[](const BearListLongInt& index)
+		T& operator[](const BearListLongInt& Index) const
 		{
-			return (T&)*GetFromIndex(index);
+			return (T&)GetFromIndex(Index);
 		}
 	};
 }
